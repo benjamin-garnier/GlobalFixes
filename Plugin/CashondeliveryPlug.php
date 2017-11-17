41c42fb929c9da9e4d671d34bf3ab52ea2392717
benjamin-garnier <benjamin.garnier1990@gmail.com>
2017-11-17 17:07:50 +0100

    Update CashondeliveryPlug.php
    Control zip code using helper class

--- Plugin/CashondeliveryPlug.php
+++ Plugin/CashondeliveryPlug.php
@@ -1,37 +1,53 @@
 <?php
-    namespace Ethos\GlobalFixes\Plugin;
-    use Magento\Payment\Model\Method\AbstractMethod;
-    use Magento\Quote\Model\Quote;
-    class CashondeliveryPlug
+
+namespace Ethos\GlobalFixes\Plugin;
+
+use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\Quote\Model\Quote;
+use Ethos\GlobalFixes\Helper\Data as ShippingHelper;
+
+class CashondeliveryPlug
+{
+    /**
+     * @var ShippingHelper $shippingHelper
+     */
+    protected $shippingHelper;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param ShippingHelper $shippingHelper
+     */
+    public function __construct
+    (
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        ShippingHelper $shippingHelper
+    )
     {
+        $this->logger = $logger;
+        $this->_checkoutSession = $checkoutSession;
+        $this->shippingHelper = $shippingHelper;
 
-      /**
-       * @var \Magento\Checkout\Model\Session
-       */
-       protected $_checkoutSession;
-
-      /**
-       * Constructor
-       *
-       * @param \Magento\Checkout\Model\Session $checkoutSession
-       */
-        public function __construct
-        (
-            \Psr\Log\LoggerInterface $logger,
-            \Magento\Checkout\Model\Session $checkoutSession
-         ) {
-            $this->logger = $logger;
-            $this->_checkoutSession = $checkoutSession;
-            return;
-        }
-
-        public function aroundIsAvailable(\Magento\Payment\Model\Method\AbstractMethod $subject, callable $proceed)
-        {
-	    $shippingMethod = $this->_checkoutSession->getQuote()->getShippingAddress()->getShippingMethod();
-            if ($shippingMethod == 'colissimo_pickup') {
-                return false;
-            }
-            $result = $proceed();
-            return $result;
-          }
+        return;
     }
+
+    public function aroundIsAvailable(\Magento\Payment\Model\Method\AbstractMethod $subject, callable $proceed)
+    {
+        $shippingMethod = $this->_checkoutSession->getQuote()->getShippingAddress()->getShippingMethod();
+        $country = $this->_checkoutSession->getQuote()->getShippingAddress()->getCountryId();
+        $postcode = $this->_checkoutSession->getQuote()->getShippingAddress()->getPostcode();
+        $realCountry = $this->shippingHelper->getCountry($country, $postcode);
+        if ($realCountry != "FR" || $shippingMethod == 'colissimo_pickup') {
+            return false;
+        }
+        $result = $proceed();
+        return $result;
+    }
+}
